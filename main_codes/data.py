a9bc856e548465bcc8cb6c90b3c96bd629b6f860
Sudipto Baul <sudipto174@gmail.com>
2026-01-20 13:34:09 -0500

    Update data.py

--- main_codes/data.py
+++ main_codes/data.py
@@ -1,5 +1,6 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
+# Copied and modified Graphormer code.
 
 from collator import collator
 from wrapper import MyGraphPropPredDataset, MyPygPCQM4MDataset, MyZINCDataset
